b7a3bbb28fecbc4f9f8e1dda9c598165daad053e
Gagandeep_singh <singh.gagandeep0034@gmail.com>
2019-09-24 13:53:29 -0500

    Added Navbar

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,14 @@
 import React from 'react'
-
+import { ProductConsumer } from '../context';
 export default function HomePage() {
     return (
         <>
-            <h1>HEllo from HomePage</h1>
+            <ProductConsumer>
+                {value => {
+                    console.log(value)
+                    return <h1>Homepage hello</h1>
+                }}
+            </ProductConsumer>
         </>
     );
 }
