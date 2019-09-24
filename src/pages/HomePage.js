6b70c75b3617d4a173941d7fc9bad5d738a36a0c
Gagandeep_singh <singh.gagandeep0034@gmail.com>
2019-09-24 17:42:25 -0500

    Added everything

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,14 +1,18 @@
-import React from 'react'
-import { ProductConsumer } from '../context';
+import React from "react";
+import Hero from "../components/Hero";
+import { Link } from "react-router-dom";
+import Services from "../components/HomePage/Services";
+import Featured from "../components/HomePage/Featured";
 export default function HomePage() {
-    return (
-        <>
-            <ProductConsumer>
-                {value => {
-                    console.log(value)
-                    return <h1>Homepage hello</h1>
-                }}
-            </ProductConsumer>
-        </>
-    );
+  return (
+    <>
+      <Hero title="" max="true">
+        <Link to="/products" className="main-link" style={{ margin: "2rem" }}>
+          our products
+        </Link>
+      </Hero>
+      <Services />
+      <Featured />
+    </>
+  );
 }
